d0d86b47a9f13d7236f2f422fc7884f86e07859d
Dipanshu Pandey <dps.coders@gmail.com>
2024-06-05 13:10:17 +0530

    GSAP animation added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,50 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const boxRefs = useRef([]);
+  boxRefs.current = [];
+  const textRef = useRef();
+
+  useEffect(()=>{
+     gsap.from(boxRefs.current,{
+       y:-1500,
+       duration:2,
+       delay:1,
+       stagger:0.3
+     })
+
+     gsap.from(textRef.current,{
+      y:500,
+      duration:1.5,
+      scrollTrigger:{
+        trigger:textRef.current,
+        markers:true,
+        scroller:'body',
+        start:'top 150%'
+      }
+     })
+    
+   },[])
+
+   const addToRefs = (el) => {
+    if (el && !boxRefs.current.includes(el)) {
+      boxRefs.current.push(el);
+    }
+  };
+
   return (
     <>
 
-    <div className='box-container flex flex-wrap items-center justify-center p-4 gap-4 mob:p-12 mob:gap-12  bg-yellow-50'>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+<div className='box-container flex flex-wrap items-center justify-center p-4 gap-4 mob:p-12 mob:gap-12 bg-yellow-50'>
+        {Array.from({ length: 8 }, (_, i) => (
+          <div key={i} ref={addToRefs} className="box border-2 bg-white border-red-400 rounded-xl w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px]">
+            <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
+            <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          </div>
+        ))}
       </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-      <div className="box border-2 bg-white border-red-400 rounded-xl  w-[140px] h-[200px] xsm:w-[180px] xsm:h-[240px] ">
-        <img src="https://imgs.search.brave.com/NjUZiET-9D8rLOEuq7YJ6P9tU8axeFDcpsDH8zATSSo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/YmVmdW5reS5jb20v/aW1hZ2VzL3ByaXNt/aWMvYThmODc3ZmYt/ZjE1My00MjMwLTlk/OTUtYjk2MGJlM2U0/YTc5X3Bob3RvLXRv/LXBhaW50aW5nLWFm/dGVyLmpwZWc_YXV0/bz1hdmlmLHdlYnAm/Zm9ybWF0PWpwZyZ3/aWR0aD04NjM" alt="" />
-        <p className='text-xl leading-6 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      </div>
-    </div>
 
-    <div className="blob w-[100vw] h-[20em] bg-[url('/blob.svg')] bg-no-repeat opacity-85 border border-green-300 flex items-center justify-center  mob:h-[32em] tab:h-[42em] ">
+    <div ref={textRef}  className="blob w-[100vw] h-[20em] bg-[url('/blob.svg')] bg-no-repeat opacity-85 border flex items-center justify-center  mob:h-[32em] tab:h-[42em] ">
       <p className="text-[2.3em] font-['montserrat'] font-bold  opacity-80 mob:text-[3em] tab:text-[4.3em]  ">
         We provide <br/>the best  painters <br/> in the world
       </p>
     </div>
 
+    
   </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,30 @@
-
-import React, { useState } from 'react'
+import {gsap} from 'gsap';
+import React, { useEffect, useRef, useState } from 'react'
 export default function Navbar() {
 
-  const[navToggle,setNavToggle]= useState(false);
+const[navToggle,setNavToggle]= useState(false);
+
   function handleToggle(){
     setNavToggle(prev=>!prev);
     
   }
-  console.log(navToggle)
+  const navRef=useRef(null);
+useEffect(()=>{
+  
+  gsap.from(navRef.current,{
+    y:-100,
+    duration:0.6,
+    opacity:0,
+    delay:1,
+    stagger:1
+  })
+})
+  
+  
+
   return (
-    <div className='sticky top-0 w-full z-10'>
-      <nav className='  w-full  bg-black p-2 px-8 border-2 flex justify-between items-center mob:p-2 mob:px-16 tab:px-12'>
+    <div ref={navRef} className='sticky top-0 w-full z-10'>
+      <nav  className='  w-full  bg-black p-2 px-8 border-2 flex justify-between items-center mob:p-2 mob:px-16 tab:px-12'>
 
          <div className=" logo h-10 w-10  mob:w-14 mob:h-14  object-cover">
             <img src='../public/logo.png' alt="logo" className='h-full rounded-full '/>
@@ -26,7 +40,7 @@ export default function Navbar() {
          { 
          navToggle
             ?
-         <div className="cross   hover:cursor-pointer">
+         <div className="cross hover:cursor-pointer">
             <svg className='h-6 w-6' fill="#ffffff"  version="1.1"    viewBox="0 0 490 490"  stroke="#ffffff"><g id="SVGRepo_bgCarrier" ></g><g id="SVGRepo_tracerCarrier" ></g><g id="SVGRepo_iconCarrier"> <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "></polygon> </g></svg>
           </div>
             :
